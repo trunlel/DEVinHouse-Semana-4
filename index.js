3048635a59c703a71db9081b8e1dda5102c771e6
Darlan Martins <langodarlan46@gmail.com>
2022-07-24 15:11:36 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let contasClientes = [
+  {
+    id: 1,
+    nome: "Cliente 01",
+    saldo: 500,
+  },
+  {
+    id: 2,
+    nome: "Cliente 02",
+    saldo: 3000,
+  },
+  {
+    id: 3,
+    nome: "Cliente 03",
+    saldo: 5000,
+  },
+];
+
+const selectConta = document.getElementById("conta");
+const strongMensagem = document.getElementById("mensagem");
+const form = document.getElementById("form");
+const body = document.body;
+
+const adicionaOpcao = (value, text) => {
+  const option = document.createElement("option");
+  option.value = value;
+  option.textContent = text;
+  option.id = value;
+
+  selectConta.appendChild(option);
+};
+
+const populaSelect = () => {
+  selectConta.innerHTML = "";
+  adicionaOpcao("0", "(Selecione)");
+
+  contasClientes.forEach((conta) => {
+    adicionaOpcao(conta.id, conta.nome);
+  });
+};
+
+body.onload = populaSelect;
